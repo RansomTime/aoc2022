9cdc6789dd0f0dbdb51b95916f0decf7c34e2c55
Alex <alex@ransomti.me>
2022-12-01 23:53:43 +0000

    solution
    fixup

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -2,30 +2,83 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let mut file = File::open("inputs/demo")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    print!("{}",contents);
-    Ok(())
+  print!("part 1: {}\n", part_1());
+  print!("part 2: {}\n", part_2());
+  Ok(())
+}
+
+fn part_1() -> i32 {
+  let mut res = 0;
+  let mut file = File::open("inputs/input").unwrap();
+  let mut contents = String::new();
+  file.read_to_string(&mut contents).unwrap();
+  for e in contents.split("\n\n") {
+    let candidate = get_calories(e.to_string());
+    if candidate > res { 
+      res = candidate;
+    }
+  }
+  
+  res
+}
+
+fn part_2() -> i32 {
+  let mut res = vec![];
+  let mut file = File::open("inputs/input").unwrap();
+  let mut contents = String::new();
+  file.read_to_string(&mut contents).unwrap();
+  for e in contents.split("\n\n") {
+    res.push(get_calories(e.to_string()));
+  }
+
+  res.sort_by(|a, b| b.cmp(a));
+  res[0] + res[1] + res[2]
+
 }
 
 fn get_calories(input: String) -> i32 {
-    if input == String::from("") {
-        return 0;
-    }
-    input.parse::<i32>().unwrap()
+  let mut res = 0;
+  for e in input.split("\n") {
+    res += e.parse::<i32>().unwrap_or(0);
+  }
+  res
+  
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::get_calories;
-    #[test]
-    fn test_get_calories_no_input() {
-        assert_eq!(get_calories(String::from("")), 0)
+  use crate::get_calories;
+  use std::fs::File;
+  use std::io::prelude::*;
+  
+  #[test]
+  fn test_get_calories_no_input() {
+    assert_eq!(get_calories(String::from("")), 0)
+  }
+  
+  #[test]
+  fn test_get_calories_single_number() {
+    assert_eq!(get_calories(String::from("1000")), 1000)
+  }
+  
+  #[test]
+  fn test_get_calories_multiple_number() {
+    assert_eq!(get_calories(String::from(
+      "1000\n1000")), 2000)
     }
-
+    
     #[test]
-    fn test_get_calories_single_number() {
-        assert_eq!(get_calories(String::from("1000")), 1000)
+    fn test_get_calories_demo() {
+      let mut file = File::open("inputs/demo").unwrap();
+      let mut contents = String::new();
+      file.read_to_string(&mut contents).unwrap();
+      let mut i = 0;
+      let expected = vec![6000, 4000, 11000, 24000, 10000];
+      for e in contents.split("\n\n") {
+        assert_eq!(get_calories(e.to_string()),expected[i]);
+        i += 1;
+      }
+      
     }
-}
+  }
+  
