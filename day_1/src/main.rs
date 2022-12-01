b33b04d032648b5af783347f595ed132a6553ac2
Alex <alex@ransomti.me>
2022-12-01 23:29:27 +0000

    step

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -10,14 +10,22 @@ fn main() -> std::io::Result<()> {
 }
 
 fn get_calories(input: String) -> i32 {
-    0
+    if input == String::from("") {
+        return 0;
+    }
+    input.parse::<i32>().unwrap()
 }
 
 #[cfg(test)]
 mod tests {
     use crate::get_calories;
     #[test]
-    fn test_get_calories() {
+    fn test_get_calories_no_input() {
         assert_eq!(get_calories(String::from("")), 0)
+    }
+
+    #[test]
+    fn test_get_calories_single_number() {
+        assert_eq!(get_calories(String::from("1000")), 1000)
     }
 }
