e4971edb4a9fab69bfc155f9e1a455177be02db2
Alex Bryson <alex@ransomti.me>
2022-12-03 16:10:32 +0000

    Allow dead code on helper function for linter

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,12 +1,24 @@
 use std::fs::File;
 use std::io::prelude::*;
 
+#[allow(dead_code)]
 fn file_to_string(path: &str) -> String {
   let mut file = File::open(path).unwrap();
   let mut contents = String::new();
   file.read_to_string(&mut contents).unwrap();
   contents
 }
+
+
+
+fn char_to_score(input: char) -> u32 {
+    match input {
+        'a'..='z' => <char as Into<u32>>::into(input) - 96,
+        'A'..='Z' => 27,
+        _ => panic!("char_to_score({}) expected [a-zA-Z]",input),
+    }
+}
+
 
 fn main() {
     println!("part 1: {}", part_1());
@@ -25,10 +37,13 @@ fn part_2() -> i32 {
 
 #[cfg(test)]
 mod test {
-    use crate::inputs;
+    use crate::*;
 
     #[test]
-    fn dummy_test() {
-        assert_eq!("", "");
+    fn score_char() {
+        //let test:char = 'a';
+        assert_eq!(char_to_score('a'), 1);
+        assert_eq!(char_to_score('b'), 2);
+        assert_eq!(char_to_score('A'), 27);
     }
 }
