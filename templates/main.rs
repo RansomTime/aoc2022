ebb5a74caf8dd65eaea444a66e1bd35a6c7be8fb
Alex Bryson <alex@ransomti.me>
2022-12-06 18:19:04 +0000

    update template

--- templates/main.rs
+++ templates/main.rs
@@ -1,24 +1,15 @@
-use std::fs::File;
-use std::io::prelude::*;
-
-#[allow(dead_code)]
-fn file_to_string(path: &str) -> String {
-  let mut file = File::open(path).unwrap();
-  let mut contents = String::new();
-  file.read_to_string(&mut contents).unwrap();
-  contents
-}
+const INPUT: &str = include_str!("../inputs/input");
 
 fn main() {
     println!("part 1: {}", part_1());
     println!("part 2: {}", part_2());
 }
 
-fn part_1() -> i32 {
+fn part_1() -> u32 {
     0
 }
 
-fn part_2() -> i32 {
+fn part_2() -> u32 {
     0
 }
 
@@ -26,7 +17,6 @@ fn part_2() -> i32 {
 
 #[cfg(test)]
 mod test {
-    use crate::inputs;
 
     #[test]
     fn dummy_test() {
