bc09502f9d8538d4c4a93e76cc671103b6ee2605
Alex Bryson <alex@ransomti.me>
2022-12-07 19:41:39 +0000

    working

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,5 +1,5 @@
 use std::cell::RefCell;
-use std::ops::Deref;
+use std::ops::DerefMut;
 use std::rc::Rc;
 
 const INPUT: &str = include_str!("../inputs/input");
@@ -9,12 +9,123 @@ fn main() {
     println!("part 2: {}", part_2());
 }
 
-fn part_1() -> u32 {
-    0
+fn part_1() -> i32 {
+    let root = Rc::from(RefCell::from(DirectoryTree::new_root()));
+    let mut pwd = Rc::clone(&root);
+    for line in INPUT.split('\n') {
+        if line.starts_with("$ cd /") {
+            pwd = Rc::clone(&root);
+            continue;
+        }
+        if line.starts_with("$ ls") {
+            continue;
+        }
+        if line.starts_with("$ cd ..") {
+            let next = Rc::clone(&pwd.borrow_mut().get_parent());
+            pwd = next;
+            continue;
+        }
+        if line.starts_with("$ cd ") {
+            let search = line.strip_prefix("$ cd ").unwrap();
+            let pwd_ = Rc::clone(&pwd);
+            // telling rust softly that I love it
+            // and I'm not going to ruin it's nicely allocated memory
+            let mut canary = true;
+            for child in &pwd_.borrow().children {
+                if child.borrow().name == *search {
+                    pwd = Rc::clone(child);
+                    canary = false;
+                    break;
+                }                     
+            }
+            if canary {
+                panic!("squark");
+            }
+            continue;
+        }
+        if line.starts_with("dir ") {
+            let new_name = line.strip_prefix("dir ").unwrap();
+            let next = Rc::clone(&pwd);
+            pwd.borrow_mut().deref_mut().add_dir(new_name, next);
+            continue;
+        }
+        // else
+        let mut file = line.split(' ');
+        let size:i32 = file.next().unwrap().parse().unwrap();
+        let name = file.next().unwrap();
+        pwd.borrow_mut().add_file(name, size);                
+    }
+    //return root.borrow().size_of_children();
+    //assert_eq!(48381165,root.borrow().size_of_children());
+    let mut res = 0;
+    let kids = root.borrow().better_size_of_children();
+    for e in kids {
+        if e <= 100_000 {
+            res += e;
+        }
+    }
+    res
+    
 }
 
-fn part_2() -> u32 {
-    0
+fn part_2() -> i32 {
+    let root = Rc::from(RefCell::from(DirectoryTree::new_root()));
+    let mut pwd = Rc::clone(&root);
+    for line in INPUT.split('\n') {
+        if line.starts_with("$ cd /") {
+            pwd = Rc::clone(&root);
+            continue;
+        }
+        if line.starts_with("$ ls") {
+            continue;
+        }
+        if line.starts_with("$ cd ..") {
+            let next = Rc::clone(&pwd.borrow_mut().get_parent());
+            pwd = next;
+            continue;
+        }
+        if line.starts_with("$ cd ") {
+            let search = line.strip_prefix("$ cd ").unwrap();
+            let pwd_ = Rc::clone(&pwd);
+            // telling rust softly that I love it
+            // and I'm not going to ruin it's nicely allocated memory
+            let mut canary = true;
+            for child in &pwd_.borrow().children {
+                if child.borrow().name == *search {
+                    pwd = Rc::clone(child);
+                    canary = false;
+                    break;
+                }                     
+            }
+            if canary {
+                panic!("squark");
+            }
+            continue;
+        }
+        if line.starts_with("dir ") {
+            let new_name = line.strip_prefix("dir ").unwrap();
+            let next = Rc::clone(&pwd);
+            pwd.borrow_mut().deref_mut().add_dir(new_name, next);
+            continue;
+        }
+        // else
+        let mut file = line.split(' ');
+        let size:i32 = file.next().unwrap().parse().unwrap();
+        let name = file.next().unwrap();
+        pwd.borrow_mut().add_file(name, size);                
+    }
+    let unused_space = 70000000-root.borrow().size_of_children();
+    let needed_space = 30000000-unused_space;
+    //assert_eq!(48381165,root.borrow().size_of_children());
+
+    let kids = root.borrow().better_size_of_children();
+    let mut res = i32::MAX;
+    for e in kids {
+        if e >= needed_space && e <= res {
+            res = e;
+        }
+    }
+    res
 }
 #[derive(Debug)]
 struct DirectoryTree {
@@ -69,23 +180,27 @@ impl DirectoryTree {
         }
         sum
     }
+
+    fn better_size_of_children(&self) -> Vec<i32> {
+        let mut res = Vec::new();
+        res.push(self.size_of_children());
+        for child in &self.children {
+            res.extend(child.borrow().better_size_of_children());
+        }
+        res
+
+    }
 }
 
 #[derive(Debug)]
 struct DirectoryFile {
-    name: String,
+    name: String, // never used lol
     size: i32,
-}
-
-#[derive(Debug)]
-enum DirectoryItem {
-    Directory(DirectoryTree),
-    Item(DirectoryFile)
 }
 
 #[cfg(test)]
 mod test {
-    use std::ops::{Deref, DerefMut};
+    use std::ops::{DerefMut};
 
     use crate::*;
     #[test]
@@ -136,43 +251,59 @@ mod test {
         assert_eq!(root.borrow().size_of_children(),33);
     }
 
-    /*
+    
     #[test]
     fn test_part_1() {
         let root = Rc::from(RefCell::from(DirectoryTree::new_root()));
         let mut pwd = Rc::clone(&root);
         let input: &str = include_str!("../inputs/demo");
-        let mut lines = input.split('\n');
-        let mut parserMode = "cmd";
-        while let Some(line) = lines.next() {
-            if line == "$ cd /" {
+        for line in input.split('\n') {
+            if line.starts_with("$ cd /") {
                 pwd = Rc::clone(&root);
                 continue;
             }
-            if line == "$ ls" {
+            if line.starts_with("$ ls") {
                 continue;
             }
-            if line == "$ cd .." {
+            if line.starts_with("$ cd ..") {
                 let next = Rc::clone(&pwd.borrow_mut().get_parent());
                 pwd = next;
                 continue;
             }
-            /*if line.starts_with("$ cd ") {
-                let search = line.get_mut(6..).unwrap();
-                for child in pwd.borrow().children {
-                    match child.borrow_mut().deref_mut() {
-                        DirectoryItem::Directory(dir) => {
-                            if dir.name == String::from(search) {
-                                pwd = Rc::clone(&pwd);
-                            }                            
-                        },
-                        _ => (),
-                    }
+            if line.starts_with("$ cd ") {
+                let search = line.strip_prefix("$ cd ").unwrap();
+                println!("search: '{search}'");
+                let pwd_ = Rc::clone(&pwd);
+                // telling rust softly that I love it
+                // and I'm not going to ruin it's nicely allocated memory
+                for child in &pwd_.borrow().children {
+                    if child.borrow().name == *search {
+                        pwd = Rc::clone(child);
+                        break;
+                    }                     
                 }
-            }*/
-                
-            
-            print!("{}",line);
+                continue;
+            }
+            if line.starts_with("dir ") {
+                let new_name = line.strip_prefix("dir ").unwrap();
+                let next = Rc::clone(&pwd);
+                pwd.borrow_mut().deref_mut().add_dir(new_name, next);
+                continue;
+            }
+            // else
+            let mut file = line.split(' ');
+            let size:i32 = file.next().unwrap().parse().unwrap();
+            let name = file.next().unwrap();
+            pwd.borrow_mut().add_file(name, size);                
         }
-    }*/
+        assert_eq!(48381165,root.borrow().size_of_children());
+        let mut res = 0;
+        let kids = root.borrow().better_size_of_children();
+        for e in kids {
+            if e <= 100_000 {
+                res += e;
+            }
+        }
+        assert_eq!(res, 95_437)
+    }
 }
